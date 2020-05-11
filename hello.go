81ee965d065b46ae9df9d883e0a6557fb6774a7c
Romi Abdul Yusuf <romi161199@gmail.com>
2020-05-11 11:39:33 +0700

    variable

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	io "bufio"
+	f "fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// var angka int
+	// f.Print("Masukan angka : ")
+	// f.Scan(&angka)
+	// f.Println("Angka yang dimasukkan : ", angka)
+
+	scanner := io.NewScanner(os.Stdin)
+	f.Print("Masukan kalimat : ")
+	scanner.Scan()
+	f.Println("Kalimat yang dimasukan : " + scanner.Text() + "")
 }
